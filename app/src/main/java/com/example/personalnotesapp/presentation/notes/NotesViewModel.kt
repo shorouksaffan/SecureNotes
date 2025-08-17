f710e28c2aa7a08a1beb5a234d2c803fca5c879f
m7mdfox <fcb.mohamed2005@gmail.com>
2025-08-17 15:57:39 +0300

    Repository and viewModels done

--- app/src/main/java/com/example/personalnotesapp/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/personalnotesapp/presentation/notes/NotesViewModel.kt
@@ -1,2 +1,15 @@
 package com.example.personalnotesapp.presentation.notes
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.personalnotesapp.data.model.Note
+import com.example.personalnotesapp.data.repository.NoteRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+
+class NotesViewModel(private val repository: NoteRepository) : ViewModel() {
+
+    val allNotes: StateFlow<List<Note>> = repository.getAllNotes()
+        .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+}
